09429e18e99e251adaef5fb2862e6736967adfa6
Ben David Walker <benwalker92@me.com>
2015-01-19 00:03:42 +0000

    Created brand new, untouched project.

---
+++ Insulator/Insulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Insulator
+//
+//  Created by Ben Walker on 19/01/2015.
+//  Copyright (c) 2015 Ben David Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
