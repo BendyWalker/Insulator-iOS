90b17c19928814e901329fc5c162c97a42f2e954
Ben David Walker <benwalker92@me.com>
2015-01-20 15:53:57 +0000

    Incredibly basic first build.

--- Insulator/Insulator/ViewController.swift
+++ Insulator/Insulator/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentBloodGlucoseLevelTextField: UITextField!
+    @IBOutlet weak var carbohydratesInMealTextField: UITextField!
+    @IBOutlet weak var suggestedDoseLabel: UILabel!
+    
+    @IBAction func calculateDose(sender: AnyObject) {
+        let currentBloodGlucoseLevel : Double = (currentBloodGlucoseLevelTextField.text as NSString).doubleValue
+        let carbohydratesInMeal = (carbohydratesInMealTextField.text as NSString).doubleValue
+        
+        let calculator = Calculator(currentBloodGlucoseLevel: currentBloodGlucoseLevel, carbohydratesInMeal: carbohydratesInMeal)
+        let suggestedDose : String = "\(calculator.getSuggestedDose(true))"
+        
+        suggestedDoseLabel.text = suggestedDose
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
