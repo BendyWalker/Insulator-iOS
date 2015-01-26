e14ce42d513358cbc9ed3467d565de1d9f809d87
Ben David Walker <benwalker92@me.com>
2015-01-26 12:40:28 +0000

    Add links between constants scene and view controller

--- Insulator/Insulator/ConstantsViewController.swift
+++ Insulator/Insulator/ConstantsViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ConstantsViewController: UIViewController {
 
+    @IBOutlet weak var carbohydrateFactorTextField: UITextField!
+    @IBOutlet weak var correctiveFactorTextField: UITextField!
+    @IBOutlet weak var desiredBloodGlucoseTextField: UITextField!
+    
+    @IBAction func closeModal(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
