bdedb4cccd81f09d6af866cad1a2561e6a337287
Ben David Walker <benwalker92@me.com>
2015-05-04 20:09:06 +0800

    BloodGlucoseUnit TableView updates preference correctly

--- Insulator/Insulator/SettingsViewController.swift
+++ Insulator/Insulator/SettingsViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
 
 class SettingsTableViewController: UITableViewController {
+    let preferencesManager = PreferencesManager.sharedInstance
+    
+    
+    @IBOutlet weak var bloodGlucoseUnitLabel: UILabel!
     @IBOutlet weak var allowFloatingPointCarbohydratesSwitch: UISwitch!
     
     
@@ -8,19 +12,66 @@ class SettingsTableViewController: UITableViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
-    @IBAction func updateBloodGlucoseUnitPreference(sender: UISegmentedControl) {
-    }
-    
     @IBAction func updateAllowFloatingPointCarbohydratePreference(sender: UISwitch) {
     }
     
+    
+    override func viewDidLoad() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateBloodGlucoseUnitLabel", name: PreferencesDidChangeNotification, object: nil)
+    }
     
     override func viewWillAppear(animated: Bool) {
         self.tableView.estimatedRowHeight = 44
         self.tableView.rowHeight = UITableViewAutomaticDimension
     }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: PreferencesDidChangeNotification, object: nil)
+    }
+    
+    
+    func updateBloodGlucoseUnitLabel() {
+        bloodGlucoseUnitLabel.text = preferencesManager.bloodGlucoseUnit.rawValue
+    }
 }
 
+
 class BloodGlucoseUnitTableViewController: UITableViewController {
+    let preferencesManager = PreferencesManager.sharedInstance
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return BloodGlucoseUnit.count()
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("BloodGlucoseUnitCell") as! UITableViewCell
+        
+        if let bloodGlucoseUnit = BloodGlucoseUnit.fromInt(indexPath.row) {
+            cell.textLabel?.text = bloodGlucoseUnit.rawValue
+
+            if bloodGlucoseUnit == preferencesManager.bloodGlucoseUnit {
+                cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+            } else {
+                cell.accessoryType = UITableViewCellAccessoryType.None
+            }
+        }
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        return indexPath
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let bloodGlucoseUnit = BloodGlucoseUnit.fromInt(indexPath.row) {
+            preferencesManager.bloodGlucoseUnit = bloodGlucoseUnit
+        }
+        
+        tableView.reloadData()
+    }
 }
